a8efa46aa9a93c36a573b208f37d9824cd0830ed
naoya70 <152585990+naoya70@users.noreply.github.com>
2023-12-05 00:39:51 +0900

    __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('/content/ssd.pytorch/data')
+
 from .voc0712 import VOCDetection, VOCAnnotationTransform, VOC_CLASSES, VOC_ROOT
 
 from .coco import COCODetection, COCOAnnotationTransform, COCO_CLASSES, COCO_ROOT, get_label_map
